00c3373b0c48f60bcef0a824ddcef6ebb9ff30d8
vivianiHarima <viviharima@hotmail.com>
2024-06-25 22:21:35 -0300

    exercicio02

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//Exercício 01
-//Dado um array de números, crie uma função que retorne um novo array, contendo apenas os números pares.
+/*Exercício 01
+Dado um array de números, crie uma função que retorne um novo array, contendo apenas os números pares.*/
 
 function filtrarPares(numeros) {
 return numeros.filter(numero => 
@@ -9,5 +9,32 @@ return numeros.filter(numero =>
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(filtrarPares(numeros));
+
+
+
+/*Exercício 02
+Dado um array de objetos representando pessoas com propriedades nome e idade,
+crie uma função que retorne um array, contendo apenas os nomes das pessoas com idade maior ou igual a 18 anos.*/
+
+
+let pessoas = [
+    { nome: 'Alice', idade: 17 },
+    { nome: 'Bob', idade: 22 },
+    { nome: 'Charlie', idade: 16 },
+    { nome: 'David', idade: 19 }
+    ];
+
+
+function filtrarAdultos(pessoas) {
+        let pessoasMaior = pessoas.filter(pessoa =>
+            pessoa.idade>=18);
+        let nomesPessoasMaior = pessoasMaior.map(pessoa => pessoa.nome)
+            return nomesPessoasMaior
+    }
+
+
+
+
+console.log(filtrarAdultos(pessoas)); 
 
 
